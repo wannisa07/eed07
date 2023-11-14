4713cc00c8c09e118e1a48e04533c08e0c8129f1
wannisa07 <wannisakoonlesa50@gmail.com>
2023-11-14 11:05:05 +0700

    submit.php

---
+++ pages/submit.php
@@ -0,0 +1,23 @@
+<?php
+    $_POST =  $_POST['email'];
+    $password =  $_POST['passeord'];
+    $city =  $_POST['city'];
+    $web =  $_POST['web'] ;
+    $role =  $_POST['role'];
+    $sing1 =  $_POST['sing1'];
+    $sing2 = $_POST['sing2'];
+    $sing3 = $_POST['sing3'];
+
+
+    echo 'email: '.$email. '<br>';
+    echo 'password: '.$password.'<br>';    
+    echo 'city: '.$city.'<br>';
+    echo 'web: '.$web. '<br>';
+    echo 'role: '.$role. '<br>'
+    echo 'sing1: '.$sing1. '<br>'
+    echo 'sing2: '.$sing2. '<br>'
+    echo 'sing3: '.$sing3. '<br>'
+    
+    
+
+    ?>

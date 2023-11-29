ea9cc1f2e14232dcddb6a26eb366430aa1b4cf6b
wannisa07 <wannisakoonlesa50@gmail.com>
2023-11-29 10:16:12 +0700

    การสร้างเว็บไซต์

---
+++ LinkExampie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="week3Link.html">กลับหน้าหลัก</a>
+    <h1>ตัวอย่างการใช้งาน Tag a</h1>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat non, est nemo ipsum, consectetur unde quaerat asperiores quo nisi, dolorum modi voluptatibus at. Optio odio possimus error quos mollitia ullam!</p>
+
+
+
+    
+</body>
+</html>
